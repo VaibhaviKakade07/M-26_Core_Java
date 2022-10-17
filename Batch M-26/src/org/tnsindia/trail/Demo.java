24fdbd5d75ce0632d272b213fd8a714b67b234a0
manasikakade <manasikakade@192.168.0.7>
2022-10-17 17:59:01 +0530

    commit

---
+++ Batch M-26/src/org/tnsindia/trail/Demo.java
@@ -0,0 +1,11 @@
+package org.tnsindia.trail;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Vaibhavi Amar Kakade");
+		// TODO Auto-generated method stub
+
+	}
+
+}
